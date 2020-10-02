9a7634a4d1b87b66875f5084912c1d64dda8c40d
Albert Ahn <58611720+albertfromsd@users.noreply.github.com>
2020-10-02 15:33:44 -0700

    Basic Landing Page layout initialized

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
 
+// [ COMPONENTS ]
+import Nav from './components/Nav/Nav';
+import Footer from './components/Footer/Footer';
+import MainViewContainer from './views/MainViewContainer/MainViewContainer';
+
+// [ VIEWS ]
+
 import './styles/sass/main.scss';
 
 function App() {
   return (
-    <div className={`app-wrapper`}>
-        Main App
+    <div className={`app-wrapper u-margin-top-sm`}>
+        <Nav />
+        <MainViewContainer />
+        <Footer />
     </div>
   );
 }
---
+++ client/src/views/MainViewContainer/MainViewContainer.js
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from 'react';
+
+// [ REDUX ]
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+
+// [ STYLING ]
+import '../../styles/sass/main.scss';
+
+// [ COMPONENTS ]
+
+
+// [ VIEWS ]
+
+
+
+// [ MAIN ]
+const MainViewContainer = ( props ) => {
+    
+    return (
+        <div className={`mainview`}>
+            MainViewContainer
+        </div>
+    )
+};
+
+
+
+// [ REDUX CONNECT ]
+const mapStateToProps = createStructuredSelector({
+    
+    
+});
+
+const mapDispatchToProps = dispatch => ({
+    
+    
+});
+
+
+export default connect( mapStateToProps, mapDispatchToProps )( MainViewContainer );
