7557ec1674dcde3abf593945bf060bcdfed6ac2b
shijie liu <shijieliu@shijies-MacBook-Air.local>
2020-10-22 16:52:21 -0700

    tried to merge after albert google login

--- client/src/App.js
+++ client/src/App.js
@@ -2,10 +2,10 @@ import React, { useEffect } from "react";
 import { Route, Switch, Redirect } from "react-router-dom";
 
 // [ REDUX ]
-import { connect } from 'react-redux';
-import { createStructuredSelector } from 'reselect';
-import { selectCurrentUser } from './redux/user/user.selectors';
-import { setCurrentUser } from './redux/user/user.actions';
+import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
+import { selectCurrentUser } from "./redux/user/user.selectors";
+import { setCurrentUser } from "./redux/user/user.actions";
 
 // styles
 import "assets/scss/main.scss";
@@ -23,47 +23,46 @@ import Sections from "views/Sections.js";
 import ApiTest from "views/examples/ApiTest.js";
 import AboutUs from "views/examples/AboutUs.js";
 import ContactUs from "views/examples/ContactUs.js";
-// import Ecommerce from "views/examples/Ecommerce.js";
+import Ecommerce from "views/examples/Ecommerce.js";
 import ProductPage from "views/examples/ProductPage.js";
 import AccountSettings from "views/examples/AccountSettings.js";
 import RegisterPage from "views/examples/RegisterPage.js";
 import CheckoutPage from "views/examples/CheckoutPage.js";
-import ApiTest from "components/ApiTest/ApiTest";
 
 // [ COMPONENTS ]
-import LoginPage from 'views/LoginRegPage/LoginPage';
+import LoginPage from "views/LoginRegPage/LoginPage";
 
 // [ VIEWS ]
 
-const App = ( props, { setCurrentUser, currentUser } ) => {
-    
-    // useEffect( () => {
-    //     const query = queryString.parse( props.location.search );
+const App = (props, { setCurrentUser, currentUser }) => {
+	// useEffect( () => {
+	//     const query = queryString.parse( props.location.search );
 
-    //     if( query.token ) {
-    //         console.log( "<GoogleLogin /> ", query.token );
-    //         setCurrentUser(query.token);
-            
-    //         window.localStorage.setItem( "jwt", query.token );
-    //         props.history.push("/");
-    //     } else {
-    //         console.log( "token not created. query: ", query );
-    //     }
-    // }, [currentUser, setCurrentUser]);
+	//     if( query.token ) {
+	//         console.log( "<GoogleLogin /> ", query.token );
+	//         setCurrentUser(query.token);
+
+	//         window.localStorage.setItem( "jwt", query.token );
+	//         props.history.push("/");
+	//     } else {
+	//         console.log( "token not created. query: ", query );
+	//     }
+	// }, [currentUser, setCurrentUser]);
 
 	return (
 		<div>
 			<Switch>
-				<Route path='/presentation' render={(props) => <Presentation {...props} />} />
+				<Route
+					path='/presentation'
+					render={(props) => <Presentation {...props} />}
+				/>
 				<Route path='/sections' render={(props) => <Sections {...props} />} />
 				<Route path='/about-us' render={(props) => <AboutUs {...props} />} />
-<<<<<<< HEAD
 				<Route
 					path='/contact-us'
 					render={(props) => <ContactUs {...props} />}
 				/>
-
-				<Route path='/apiTest' render={(props) => <ApiTest {...props} />} />
+				<Route path='/ecommerce' render={(props) => <Ecommerce {...props} />} />
 				<Route
 					path='/product-page'
 					render={(props) => <ProductPage {...props} />}
@@ -71,10 +70,6 @@ const App = ( props, { setCurrentUser, currentUser } ) => {
 				<Route
 					path='/account-settings'
 					render={(props) => <AccountSettings {...props} />}
-				/>
-				<Route
-					path='/login-page'
-					render={(props) => <LoginPage {...props} />}
 				/>
 				<Route
 					path='/register-page'
@@ -85,33 +80,25 @@ const App = ( props, { setCurrentUser, currentUser } ) => {
 					render={(props) => <CheckoutPage {...props} />}
 				/>
 				<Route path='/apitest' render={(props) => <ApiTest {...props} />} />
+
+				<Route
+					path='/login-page'
+					render={(props) => <LoginPage {...props} />}
+				/>
+
 				<Route path='/' render={(props) => <Index {...props} />} />
 				<Redirect to='/' />
-=======
-				<Route path='/contact-us' render={(props) => <ContactUs {...props} />} />
-				<Route path='/ecommerce' render={(props) => <Ecommerce {...props} />} />
-				<Route path='/product-page' render={(props) => <ProductPage {...props} />} />
-				<Route path='/account-settings' render={(props) => <AccountSettings {...props} />} />
-				<Route path='/register-page' render={(props) => <RegisterPage {...props} />} />
-				<Route path='/checkout-page' render={(props) => <CheckoutPage {...props} />} />
-				<Route path='/apitest' render={(props) => <ApiTest {...props} />} />
-				
-                <Route path='/login-page' render={(props) => <LoginPage {...props} />} />
-                
-                <Route path='/' render={ (props) => <Index {...props} /> } />
-                <Redirect to='/' />
->>>>>>> c14430af5a20d389432fd07c8d91f1e1d0cea061
 			</Switch>
 		</div>
 	);
 };
 
 const mapStateToProps = createStructuredSelector({
-    currentUser: selectCurrentUser
+	currentUser: selectCurrentUser,
 });
 
-const mapDispatchToProps = dispatch => ({
-    setCurrentUser: user => dispatch( setCurrentUser(user) )
+const mapDispatchToProps = (dispatch) => ({
+	setCurrentUser: (user) => dispatch(setCurrentUser(user)),
 });
 
-export default connect( mapStateToProps, mapDispatchToProps )(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
